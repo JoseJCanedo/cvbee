22cb4d0231e40f64b4b60e5aad5fbc69f117c2dd
Jose Canedo <jose@Joses-MacBook-Pro.local>
2025-05-12 09:46:31 -0400

    g

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -11,8 +11,9 @@ export default function Banner() {
     const withinFiveDays = today.getMonth() === calcDate.getMonth() && ((today.getDay() - calcDate.getDay()) <= 0);
 
     if (withinFiveDays) {
-        const meetingLocation = (today.getMonth() + 1) % 2 === 0 ? 'Striplin Terrace UMC' : 'Oxbow Meadows' //%20
-        const directions = (today.getMonth() + 1) % 2 === 0 ? '32.523631501341896%2C%20-84.92014205292037' : '32.386370054391094%2C%20-84.95877673226478'
+        const meetingLocation = (today.getMonth() + 1) % 2 === 0 ? 'Striplin Terrace UMC' : 'Oxbow Meadows' 
+        // const directions = (today.getMonth() + 1) % 2 === 0 ? '32.523631501341896%2C%20-84.92014205292037' : '32.386370054391094%2C%20-84.95877673226478'
+        const directions = (today.getMonth() + 1) % 2 === 0 ? 'Striplin%20Terrace%20UMC%20Columbus%20GA' : 'Oxbow%20Meadows%20Columbus%20GA'
         return (
             <div className="relative isolate justify-center flex items-center gap-x-6 overflow-hidden bg-yellow-100 px-6 py-2.5 sm:px-3.5 ">
                 <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
