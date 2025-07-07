6506d3bc2349c847f4ec00d0a7fa16324c59eff9
Jose Canedo <jose@Joses-MacBook-Pro.local>
2025-07-06 22:33:58 -0400

    gsearch

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="google-site-verification" content="MqJmv4Tfd7K60rsru5ckhuMCcq9zBIvYx1fsyNvWL-A" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
