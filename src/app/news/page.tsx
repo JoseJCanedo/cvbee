d01586b1e7681390752db191ec66157999925b75
Jose Canedo <jose@Joses-MacBook-Pro.local>
2025-10-07 13:59:43 -0500

    g

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Card, CardBody, CardHeader, HeroUIProvider } from "@heroui/react";
-import { getCurrentAndNextSecondMondays } from "@/utils/dates";
+// import { getCurrentAndNextSecondMondays } from "@/utils/dates";
 
 const news = [
   {
@@ -10,10 +10,41 @@ const news = [
     content: 'A club member inherited a gem of an observation hive and used it for many years. He donated it to the club and bidding will start at $10. Submit your bid by sending an email to the club secretary. When you submit a bid you will be notified of the current bid. The auction will close the day of the October Picnic.',
     author: 'Club Secretary'
   },
+  {
+    id: '2',
+    title: 'Christmas Tree Decoration',
+    date: '9/15/2025',
+    content: 'We need CVBA members to volunteer to decorate the Uptown Christmas tree this year if we are going to participate. We won last year and several other years in the past and we get a fair bit of great publicity for the club from the event. However, we need volunteers to be able to do this event. We have to order out tree no later than Nov 15th, 2025 if we are to participate. Decorating days would be December 3rd, and 4th from 10:00 AM - 5:00 PM. The lighting of the trees is scheduled for December 5th from 6:00 - 7:00 PM. Then the tree should be un-decorated no later than January 5th, 2026. If no one is interested in participating the club will not participate.',
+    author: 'Club Secretary'
+  },
+  {
+    id: '3',
+    title: 'Bi-City Columbus Parade',
+    date: '9/15/2025',
+    content: 'We need people to volunteer for the parade. The parade is schedule for December 6th, 2025 at 10:00 AM. The route starts at Colin Powell Parkway in Phenix City and we would need to be there by 8:30 AM. We are looking for members who would like to be decorators and riders as well. Come dressed in your beekeeping suits or as bees. A sign up sheet will be available at the picnic.',
+    author: 'Club Secretary'
+  },
 ]
 
-export default function Home() {
+interface EventProps {
+  id: string;
+  title: string;
+  date: string;
+  content: string;
+  author: string;
+}
 
+function sortByDate(events: EventProps[]): EventProps[] {
+  return [...events].sort((a, b) => {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+    return dateB.getTime() - dateA.getTime();
+  });
+}
+
+
+export default function Home() {
+  const sortedNews = sortByDate(news)
   return (
     <HeroUIProvider>
       <div
@@ -31,7 +62,7 @@ export default function Home() {
       </div>
       <div className='flex justify-center flex-col w-full'>
 
-        {news.map((item, i) => (
+        {sortedNews?.map((item, i) => (
           <Card key={i} className='max-w-[1100px] mx-2 w-full mt-8 p-8 text-black bg-zinc-100 mx-auto mb-10'>
             <CardHeader>
               <h2 className='text-xl font-bold'>
